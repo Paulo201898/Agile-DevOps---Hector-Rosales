5f0ece2436067b0f71f4f6d0272446db86366c1e
Paulo201898 <42985681+Paulo201898@users.noreply.github.com>
2018-11-14 20:18:47 -0200

    Update html_content_date.txt.sh

--- html_content_date.txt.sh
+++ html_content_date.txt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Document 
 echo "Regards"
-echo html_content_$hoje.txt > https://github.com/Paulo201898/Agile-DevOps---Hector-Rosales/commit/c5f2627d74a6b0027b76cadfefd81f27071f8332;
+echo html_content_$$date.txt > https://github.com/Paulo201898/Agile-DevOps---Hector-Rosales/commit/c5f2627d74a6b0027b76cadfefd81f27071f8332;
