285bbcda32f87c60137996a4bc6f047afe20828c
Paulo201898 <42985681+Paulo201898@users.noreply.github.com>
2018-11-14 20:47:15 -0200

    Update date.sh

--- date.sh
+++ date.sh
@@ -2,4 +2,4 @@
 #Date and redirection to script of downloads
 echo date="$(date +'%d-%m-%y')"
 echo html_content_$$date.txt
-echo html_content_$$date.txt > https://github.com/Paulo201898/Agile-DevOps---Hector-Rosales/commit/c5f2627d74a6b0027b76cadfefd81f27071f8332
+echo html_content_$$date.txt > https://github.com/Paulo201898/Agile-DevOps---Hector-Rosales/commit/5f0ece2436067b0f71f4f6d0272446db86366c1e
